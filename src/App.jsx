ebec88ced881895c3e0692fc759821ea2ecff840
Drac Zihper <draczihper@gmail.com>
2025-07-06 01:29:48 +0300

    Add useEffect hook and fetchData func

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
+import { useEffect, useState } from "react"
+import { shuffleArray } from "./utils/shuffle";
+import ImageGrid from "./components/ImageGrid";
+
+
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [clickedIds, setClickedIds] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+      const names = ['pikachu', 'charizard', 'mew', 'eevee', 'bulbasaur', 'gengar', 'blastoise', 'charmander', 'butterfree', 'tyranitar', 'arcanine', 'greninja'];
+      const results = await Promise.all(
+        names.map(async name => {
+          try {
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+            const data = await res.json();
+            return {
+              id: data.id,
+              name: data.name,
+              image: data.sprites.other['official-artwork'].front_default,
+            }
+          } catch (error) {
+            console.error("Failed to fetch pokemon:", error)
+          }
+        }))
+      setCharacters(shuffleArray(results))
+      } catch {
+        console.error(error)
+      }
+    }
+    fetchData();
+
+  }, [])
 
 
   return (
     <>
       <div>
-        <h1>Canto Universe</h1>
+        <ImageGrid characters={characters} />
       </div>
     </>
   )
 }
+export default App;
