762419085b2e9b8bce0447a74076ef537bda05c4
Drac Zihper <draczihper@gmail.com>
2025-07-06 01:54:33 +0300

    Add styles

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -3,7 +3,7 @@ import ImageCard from "./ImageCard"
 function ImageGrid({characters}) {
 
     return (
-        <div>
+        <div className="grid grid-cols-2, sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 w-full max-w-4xl">
               {characters.map(character => (
                 <ImageCard key={character.id} character={character}/>
             ))} 
