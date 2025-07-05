f047d3d2c9c7d30579ba6aaae8df1413b91a798a
Drac Zihper <draczihper@gmail.com>
2025-07-05 21:37:26 +0300

    Add shuffleArray func

---
+++ src/utils/shuffle.js
@@ -0,0 +1,8 @@
+// Fisher Yates shuffling algorithm
+export function shuffleArray(array) {
+    const copy = [...array]
+    for(const i = copy.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        [copy[i], copy[j]] = [copy[j], copy[i]];
+    }
+}
