634d18b5b2f6fadb96f00dea79013bb83b800ed5
Drac Zihper <draczihper@gmail.com>
2025-07-07 00:05:38 +0300

    Add onClick handler and styles

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -1,11 +1,11 @@
 import ImageCard from "./ImageCard"
 
-function ImageGrid({characters}) {
+function ImageGrid({characters, onCardClick}) {
 
     return (
-        <div className="grid grid-cols-2, sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 w-full max-w-4xl">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 w-full max-w-4xl">
               {characters.map(character => (
-                <ImageCard key={character.id} character={character}/>
+                <ImageCard key={character.id} character={character} onClick={() => onCardClick(character.name)}/>
             ))} 
         </div>
     )
