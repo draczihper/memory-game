d90d394c9cd81ba5469254865145d690380f0198
Drac Zihper <draczihper@gmail.com>
2025-07-07 00:04:54 +0300

    Add on click handler

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,8 +1,16 @@
 function ImageCard({character, onClick}){
     return (
-        <div>
-            <img src={character.image} alt={character.name} />
-            <div>{character.name}</div>
+        <div 
+        className="bg-white rounded-xl shadow-lg hover:scale-105 transition-transform duration-200 cursor-pointer max-w-36"
+        onClick={onClick}
+        >
+            <img 
+            src={character.image}
+            alt={character.name}
+            className="rounded-t-xl w-36 object-contain bg-white"
+            />
+        
+            <div className="p-2 text-center font-medium capitalize">{character.name}</div>
         </div>
     )
 }
