5965b28842d7dcde9130ca15f7c632292ba249b8
Drac Zihper <draczihper@gmail.com>
2025-07-09 22:37:47 +0300

    Edit grid layout & style

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -3,7 +3,7 @@ import ImageCard from "./ImageCard"
 function ImageGrid({characters, onCardClick}) {
 
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 w-full max-w-4xl">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 w-full max-w-4xl gap-0">
               {characters.map(character => (
                 <ImageCard key={character.id} character={character} onClick={() => onCardClick(character.name)}/>
             ))} 
