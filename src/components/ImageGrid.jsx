9adb0040bb517885b5fcc9178e60e1a4fef28703
Drac Zihper <draczihper@gmail.com>
2025-07-10 21:51:22 +0300

    Edit grid items gap

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -3,7 +3,7 @@ import ImageCard from "./ImageCard"
 function ImageGrid({characters, onCardClick}) {
 
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 w-full max-w-4xl gap-0">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 max-w-4xl gap-4 sm:gap-6 md:g-8 lg:gap-16">
               {characters.map(character => (
                 <ImageCard key={character.id} character={character} onClick={() => onCardClick(character.name)}/>
             ))} 
